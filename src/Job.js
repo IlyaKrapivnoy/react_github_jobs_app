9c5aa27b1bde81759bee439c96326d4271d30faf
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-20 16:43:37 +0200

    Job component

---
+++ src/Job.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const Job = ({ job }) => {
+    return (
+        <div>
+            {job.title}
+        </div>
+    )
+}
+
+export default Job
